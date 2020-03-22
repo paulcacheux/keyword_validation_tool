0f040464bc80932385efed03c5361b5d9358e8dc
Paul Cacheux <paulcacheux@gmail.com>
2020-03-21 20:31:16 -0700

    Bug fix

--- src/components/UploadResultBox.tsx
+++ src/components/UploadResultBox.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { State } from '../state/types';
 import { Paper, makeStyles, Button } from '@material-ui/core';
 import PublishIcon from '@material-ui/icons/Publish';
+import { uploadResultDocument } from '../api';
+import { Alert } from '@material-ui/lab';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -16,14 +18,18 @@ const useStyles = makeStyles(theme => ({
 
 export const UploadResultBox: React.FC<{ id: number }> = ({ id }) => {
     const classes = useStyles();
+
+    const [successMsg, setSuccesMsg] = useState<string | null>(null);
+    const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
     const words = useSelector((state: State) => state.words);
 
     const resultDocument: { [word: string]: number } = {};
-    let areAllTagged = false;
+    let areAllTagged = true;
     for (const word in words) {
         const info = words[word];
-        if (info.kept !== undefined) {
-            areAllTagged = true;
+        if (info.kept === undefined) {
+            areAllTagged = false;
         }
 
         if (info.kept === true) {
@@ -31,16 +37,27 @@ export const UploadResultBox: React.FC<{ id: number }> = ({ id }) => {
         }
     }
 
-    if (!areAllTagged) {
+    if (!areAllTagged || Object.keys(words).length === 0) {
         return null;
     }
 
-    const handleClick = (): void => {
-        console.log(id, resultDocument);
+    const handleClick = async (): Promise<void> => {
+        try {
+            console.log(id, resultDocument);
+            await uploadResultDocument(id, resultDocument);
+            setSuccesMsg('Successfully published document');
+            setErrorMsg(null);
+        } catch (error) {
+            setErrorMsg('Error: publishing document');
+            setSuccesMsg(null);
+        }
     };
 
     return (
         <Paper elevation={3} className={classes.root}>
+            {errorMsg && <Alert severity="error">{errorMsg}</Alert>}
+            {successMsg && <Alert severity="success">{successMsg}</Alert>}
+
             <Button
                 variant="contained"
                 color="default"
--- src/state/actions.ts
+++ src/state/actions.ts
@@ -1,14 +1,8 @@
-import {
-    LOAD_LOCAL_FILE,
-    CHANGE_RECORD_KEPT_STATE,
-    FETCH_SOURCE_DOCUMENT,
-    CLEAN_STATE_WORDS,
-    UPLOAD_RESULT_DOCUMENT,
-} from './types';
+import { LOAD_LOCAL_FILE, CHANGE_RECORD_KEPT_STATE, FETCH_SOURCE_DOCUMENT, CLEAN_STATE_WORDS } from './types';
 import { FileContent, readAsText } from '../fileUpload';
 import { State } from './types';
 import { ThunkAction } from 'redux-thunk';
-import { SourceDocumentData, fetchSourceDocument, ResultDocument, uploadResultDocument } from '../api';
+import { SourceDocumentData, fetchSourceDocument } from '../api';
 
 export enum AsyncStatus {
     SUCCESS,
@@ -31,7 +25,6 @@ export type AsyncAction<T, P> = AsyncSuccessAction<T, P> | AsyncErrorAction<T>;
 
 export type LoadLocalFileAction = AsyncAction<typeof LOAD_LOCAL_FILE, FileContent>;
 export type FetchSourceDocumentAction = AsyncAction<typeof FETCH_SOURCE_DOCUMENT, SourceDocumentData>;
-export type UploadResultDocumentAction = AsyncAction<typeof UPLOAD_RESULT_DOCUMENT, {}>;
 
 interface CleanStateWords {
     type: typeof CLEAN_STATE_WORDS;
@@ -46,7 +39,6 @@ interface ChangeRecordKeptStateAction {
 export type RootAction =
     | LoadLocalFileAction
     | FetchSourceDocumentAction
-    | UploadResultDocumentAction
     | ChangeRecordKeptStateAction
     | CleanStateWords;
 
@@ -88,17 +80,6 @@ export const fetchRemoteFile = (id: number): ThunkResult<Promise<void>> => {
             dispatch(asyncSuccess(FETCH_SOURCE_DOCUMENT, content) as FetchSourceDocumentAction);
         } catch (error) {
             dispatch(asyncError(FETCH_SOURCE_DOCUMENT, error) as FetchSourceDocumentAction);
-        }
-    };
-};
-
-export const uploadResultDocumentRequest = (id: number, doc: ResultDocument): ThunkResult<Promise<void>> => {
-    return async (dispatch): Promise<void> => {
-        try {
-            await uploadResultDocument(id, doc);
-            await dispatch(asyncSuccess(UPLOAD_RESULT_DOCUMENT, {}) as UploadResultDocumentAction);
-        } catch (error) {
-            dispatch(asyncError(UPLOAD_RESULT_DOCUMENT, error) as UploadResultDocumentAction);
         }
     };
 };
