89952d5f291ea18af6ed95ddd9a932bbf3aec6bc
Paul Cacheux <paulcacheux@gmail.com>
2020-03-22 13:18:25 -0700

    Bug fix

--- src/ManualPage.tsx
+++ src/ManualPage.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FileUploadBox } from './components/FileUploadBox';
 import { ScoringTable } from './components/ScoringTable';
 import { DownloadBox } from './components/DownloadBox';
+import { useDispatch } from 'react-redux';
+import { CLEAN_STATE_WORDS } from './state/types';
 
 export const ManualPage: React.FC = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch({ type: CLEAN_STATE_WORDS });
+    }, [dispatch]);
+
     return (
         <>
             <FileUploadBox />
